8424018aa1aa12257574a9e4b06906fbe5b81338
EpicPerson <54505527+metoo67789@users.noreply.github.com>
2021-01-16 21:17:12 -0500

    Fixed plugin
    The plugin mostly wouldn't work. Now it should work perfectly.

--- index.js
+++ index.js
@@ -1,28 +1,36 @@
 const { Plugin } = require('powercord/entities');
 const { inject, uninject } = require('powercord/injector');
+const { getModule } = require('powercord/webpack')
 const { getOwnerInstance, waitFor } = require('powercord/util');
 const { resolve } = require('path');
 
 module.exports = class BlurNSFW extends Plugin {
   async startPlugin() {
     this.loadStylesheet(resolve(__dirname, 'style.scss'));
-    await waitFor('.channelTextArea-rNsIhG');
-    const check = () => {
-      if (!this.instance.props.channel.nsfw) return;
-      else {
+    this.getChannel();
+  }
+
+  async pluginWillUnload() {
+    uninject('pog-blurnsfw')
+  }
+
+  async getChannel() {
+    const chatClass = getModule(['chat'], false).channelTextArea;
+    await waitFor(`.${chatClass}`)
+    const chat = document.querySelector(`.${chatClass}`);
+    this.blurImages(chat)
+  }
+
+  async blurImages(chat) {
+    const Channel = getOwnerInstance(chat);
+    inject('pog-blurnsfw', Channel.constructor.prototype, 'render', (_, res) => {
+
+      if (res.props.channel.nsfw) {
         var element = document.getElementsByClassName('scrollerInner-2YIMLh');
         element.item(0).classList.toggle('blur', true);
-      };
-    }
-    const updateInstance = () => this.instance = getOwnerInstance(document.querySelector('.channelTextArea-rNsIhG'));
-    updateInstance();
-    check();
-    inject('pog-blurnsfw', Object.getPrototypeOf(this.instance), 'addNSFWblur', () => {
-      updateInstance();
-      check();
+      }
+
+      return res;
     });
   }
-  pluginWillUnload() {
-    uninject('pog-blurnsfw');
-  }
-};
+}
