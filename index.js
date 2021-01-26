746a2c8d7b082aced7d5c0f377d1102aabc91e73
EpicPerson <54505527+metoo67789@users.noreply.github.com>
2021-01-25 20:59:58 -0500

    a update i guess only code change nothing else lol

--- index.js
+++ index.js
@@ -1,13 +1,11 @@
 const { Plugin } = require('powercord/entities');
 const { inject, uninject } = require('powercord/injector');
-const { getModule, React } = require('powercord/webpack')
-const { getOwnerInstance, waitFor } = require('powercord/util');
-const { resolve } = require('path');
-const settings = require('./Components/Settings')
+const { getModule, React } = require('powercord/webpack');
+const settings = require('./Components/Settings');
 
 module.exports = class BlurNSFW extends Plugin {
   async startPlugin() {
-    this.loadStylesheet(resolve(__dirname, 'style.scss'));
+    this.loadStylesheet('style.scss');
     powercord.api.settings.registerSettings(this.entityID, {
       category: this.entityID,
       label: 'Blur NSFW',
@@ -16,44 +14,35 @@ module.exports = class BlurNSFW extends Plugin {
         ...props
       })
     });
-    this.getChannel();
+    this.injectBlur();
   }
 
   pluginWillUnload() {
     powercord.api.settings.unregisterSettings(this.entityID);
-    uninject('pog-blurnsfw')
-  }
-
-  async getChannel() {
-    const chatClass = getModule(['chat'], false).channelTextArea;
-    await waitFor(`.${chatClass}`)
-    const chat = document.querySelector(`.${chatClass}`);
-    this.blurImages(chat);
+    uninject('pog-blurnsfw');
   }
 
   blurChannel() {
     const blur = this.settings.get('blurEffect', 10);
     const timing = this.settings.get('blurTiming', 1);
-    var element = document.getElementsByClassName('scrollerInner-2YIMLh');
-    element.item(0).style.setProperty('--blur-effect', `blur(${blur}px)`);
-    element.item(0).style.setProperty('--blur-timing', `${timing}s`)
-    element.item(0).classList.toggle('blur', true);
+    var element = document.querySelector('.scrollerInner-2YIMLh');
+    if (element === null) return;
+    element.style.setProperty('--blur-effect', `blur(${blur}px)`);
+    element.style.setProperty('--blur-timing', `${timing}s`);
+    element.classList.toggle('blur', true);
   }
 
-  async blurImages(chat) {
-    const Channel = getOwnerInstance(chat);
-    inject('pog-blurnsfw', Channel.constructor.prototype, 'render', (_, res) => {
-      const dm = this.settings.get('blurInDm', false);
-      const group = this.settings.get('blurInGroup', false);
-      const Blockedchannels = this.settings.get('Blocked'), blurChannels = this.settings.get('Blur')
-      const channel = Blockedchannels !== undefined ? Blockedchannels.filter((obj, idx) => obj.id === res.props.channel.id || obj.id === res.props.channel.recipients[0]) : ''
-      const Channel = blurChannels !== undefined ? blurChannels.filter((obj, idx) => obj.id === res.props.channel.id || obj.id === res.props.channel.recipients[0]) : ''
-      if (channel.length !== 0) return res;
-      else if (Channel.length !== 0) this.blurChannel()
-      else if (dm && res.props.channel.type === 1 || group && res.props.channel.type === 3) this.blurChannel()
-      else if (res.props.channel.nsfw && res.props.channel.type === 0) this.blurChannel()
-
+  async injectBlur() {
+    const channelTextArea = await getModule(m => m.type && m.type.render && m.type.render.displayName === "ChannelTextAreaContainer", false);
+    inject('pog-blurnsfw', channelTextArea.type, 'render', (_, res) => {
+      const channel = res.props.children.props.children[4].props.channel;
+      const blockedChannels = this.settings.get('Blocked') !== undefined ? this.settings.get('Blocked').filter((obj, idx) => obj.id === channel.id || obj.id === channel.recipients[0]) : '';
+      const blurChannels = this.settings.get('Blur') !== undefined ? this.settings.get('Blur').filter((obj, idx) => obj.id === channel.id || obj.id === channel.recipients[0]) : '';
+      if (blockedChannels.length !== 0) return res;
+      else if (blurChannels.length !== 0) this.blurChannel();
+      else if (this.settings.get('blurInDm') && channel.type === 1 || this.settings.get('blurInGroup') && channel.type === 3) this.blurChannel();
+      else if (channel.nsfw && channel.type === 0) this.blurChannel();
       return res;
-    });
+    })
   }
 }
