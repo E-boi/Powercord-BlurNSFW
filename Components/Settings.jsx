aaea42fbf66254381394bdc1463a6fc74e5bed68
EpicPerson <54505527+metoo67789@users.noreply.github.com>
2021-03-16 18:02:40 -0400

    discord back at it again and code formatted 👍

--- Components/Settings.jsx
+++ Components/Settings.jsx
@@ -2,118 +2,143 @@ const { React } = require('powercord/webpack');
 const { Button } = require('powercord/components');
 const { SwitchItem, SliderInput, Category } = require('powercord/components/settings');
 const TextInputWithButton = require('./TextInputWithButton');
-let blockedChannels = Array(), blurChannels = Array()
+let blockedChannels = Array(),
+	blurChannels = Array();
 
 module.exports = class Blur extends React.PureComponent {
-  constructor(props) {
-    super(props)
-    this.state = { category0Opened: false, category1Opened: false }
-  }
-  handleBlockChannel() {
-    return blockedChannels.concat('Enter a channel/user id')
-  }
-  handleBlockChannelChange = (idx, newvalue) => {
-    const channels = blockedChannels.map((channel, sidx) => {
-      if (idx !== sidx) return channel;
-      return { ...channel, id: newvalue }
-    });
-    return channels;
-  }
-  handleRemoveBlockChannel = (idx) => {
-    if (blockedChannels.length > 1) {
-      const channels = blockedChannels.filter((obj, sidx) => sidx !== idx);
-      return channels;
-    }
-  }
-  handleBlurAddChannel() {
-    return blurChannels.concat('Enter a channel/user id')
-  }
-  handleBlurChannelChange = (idx, newvalue) => {
-    const channels = blurChannels.map((channel, sidx) => {
-      if (idx !== sidx) return channel;
-      return { ...channel, id: newvalue }
-    });
-    return channels;
-  }
-  handleRemoveBlurChannel = (idx) => {
-    if (blurChannels.length > 1) {
-      const channels = blurChannels.filter((obj, sidx) => sidx !== idx);
-      return channels;
-    }
-  }
-  render() {
-    const { getSetting, toggleSetting, updateSetting } = this.props
-    blockedChannels = getSetting('Blocked'), blurChannels = getSetting('Blur')
-    if (blockedChannels === undefined || blockedChannels === null) blockedChannels = [{ id: 'Enter a channel/user id' }]
-    if (blurChannels === undefined || blurChannels === null) blurChannels = [{ id: 'Enter a channel/user id' }]
-    return <>
-      <SwitchItem
-        note='Blur images in dms'
-        value={getSetting('blurInDm', false)}
-        onChange={() => toggleSetting('blurInDm')}
-      >DM</SwitchItem>
-      <SwitchItem
-        note='Blur images in group chats'
-        value={getSetting('blurInGroup', false)}
-        onChange={() => toggleSetting('blurInGroup')}
-      >Group Chat</SwitchItem>
-      <SliderInput
-        stickToMarkers
-        minValue={1}
-        maxValue={50}
-        initialValue={getSetting('blurEffect', 10)}
-        markers={[1, 2, 3, 4, 5, 10, 15, 20, 25, 30, 35, 40, 45, 50]}
-        onValueChange={(change) => updateSetting('blurEffect', change)}
-      >Blur Effect</SliderInput>
-      <SliderInput
-        stickToMarkers
-        minValue={0.2}
-        maxValue={10}
-        initialValue={getSetting('blurTiming', 1)}
-        markers={[0.2, 0.5, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10]}
-        onValueChange={(change) => updateSetting('blurTiming', change)}
-      >Blur Timing (in seconds)</SliderInput>
-      <Category name='Block Channels' description='Block channels from getting effected.' opened={this.state.category0Opened} onChange={() => this.setState({ category0Opened: !this.state.category0Opened })}>
-        {blockedChannels.map((channel, idx) => (
-          <div>
-            <TextInputWithButton
-              type='text'
-              placeholder={'Enter a channel/user id'}
-              defaultValue={channel.id}
-              onChange={val => { updateSetting('Blocked', this.handleBlockChannelChange(idx, val)) }}
-              buttonOnClick={() => { updateSetting('Blocked', this.handleRemoveBlockChannel(idx)) }}
-              buttonText='Remove'
-              buttonIcon='fal fa-minus'
-            />
-          </div>
-        ))}
-        <Button
-          onClick={() => { updateSetting('Blocked', this.handleBlockChannel()) }}
-        >
-          Add channel
-      </Button>
-      </Category>
-      <Category name='Blur Channels' description='Blur channels that aren`t affected.' opened={this.state.category1Opened} onChange={() => this.setState({ category1Opened: !this.state.category1Opened })}>
-        {blurChannels.map((channel, idx) => (
-          <div>
-            <TextInputWithButton
-              type='text'
-              placeholder={'Enter a channel/user id'}
-              defaultValue={channel.id}
-              onChange={val => { updateSetting('Blur', this.handleBlurChannelChange(idx, val)) }}
-              buttonOnClick={() => { updateSetting('Blur', this.handleRemoveBlurChannel(idx)) }}
-              buttonText='Remove'
-              buttonIcon='fal fa-minus'
-            />
-          </div>
-        ))}
-        <Button
-          onClick={() => { updateSetting('Blur', this.handleBlurAddChannel()) }}
-        >
-          Add channel
-      </Button>
-      </Category>
-      <p style={{ color: '#b9bbbe' }}>(you will need to switch channel/dm for any changes to take effect)</p>
-    </>
-  }
-}
+	constructor(props) {
+		super(props);
+		this.state = { category0Opened: false, category1Opened: false };
+	}
+	handleBlockChannel() {
+		return blockedChannels.concat('Enter a channel/user id');
+	}
+	handleBlockChannelChange = (idx, newvalue) => {
+		const channels = blockedChannels.map((channel, sidx) => {
+			if (idx !== sidx) return channel;
+			return { ...channel, id: newvalue };
+		});
+		return channels;
+	};
+	handleRemoveBlockChannel = idx => {
+		if (blockedChannels.length > 1) {
+			const channels = blockedChannels.filter((obj, sidx) => sidx !== idx);
+			return channels;
+		}
+	};
+	handleBlurAddChannel() {
+		return blurChannels.concat('Enter a channel/user id');
+	}
+	handleBlurChannelChange = (idx, newvalue) => {
+		const channels = blurChannels.map((channel, sidx) => {
+			if (idx !== sidx) return channel;
+			return { ...channel, id: newvalue };
+		});
+		return channels;
+	};
+	handleRemoveBlurChannel = idx => {
+		if (blurChannels.length > 1) {
+			const channels = blurChannels.filter((obj, sidx) => sidx !== idx);
+			return channels;
+		}
+	};
+	render() {
+		const { getSetting, toggleSetting, updateSetting } = this.props;
+		(blockedChannels = getSetting('Blocked')), (blurChannels = getSetting('Blur'));
+		if (blockedChannels === undefined || blockedChannels === null) blockedChannels = [{ id: 'Enter a channel/user id' }];
+		if (blurChannels === undefined || blurChannels === null) blurChannels = [{ id: 'Enter a channel/user id' }];
+		return (
+			<>
+				<SwitchItem note="Blur images in dms" value={getSetting('blurInDm', false)} onChange={() => toggleSetting('blurInDm')}>
+					DM
+				</SwitchItem>
+				<SwitchItem note="Blur images in group chats" value={getSetting('blurInGroup', false)} onChange={() => toggleSetting('blurInGroup')}>
+					Group Chat
+				</SwitchItem>
+				<SliderInput
+					stickToMarkers
+					minValue={1}
+					maxValue={50}
+					initialValue={getSetting('blurEffect', 10)}
+					markers={[1, 2, 3, 4, 5, 10, 15, 20, 25, 30, 35, 40, 45, 50]}
+					onValueChange={change => updateSetting('blurEffect', change)}
+				>
+					Blur Effect
+				</SliderInput>
+				<SliderInput
+					stickToMarkers
+					minValue={0.2}
+					maxValue={10}
+					initialValue={getSetting('blurTiming', 1)}
+					markers={[0.2, 0.5, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10]}
+					onValueChange={change => updateSetting('blurTiming', change)}
+				>
+					Blur Timing (in seconds)
+				</SliderInput>
+				<Category
+					name="Block Channels"
+					description="Block channels from getting effected."
+					opened={this.state.category0Opened}
+					onChange={() => this.setState({ category0Opened: !this.state.category0Opened })}
+				>
+					{blockedChannels.map((channel, idx) => (
+						<div>
+							<TextInputWithButton
+								type="text"
+								placeholder={'Enter a channel/user id'}
+								defaultValue={channel.id}
+								onChange={val => {
+									updateSetting('Blocked', this.handleBlockChannelChange(idx, val));
+								}}
+								buttonOnClick={() => {
+									updateSetting('Blocked', this.handleRemoveBlockChannel(idx));
+								}}
+								buttonText="Remove"
+								buttonIcon="fal fa-minus"
+							/>
+						</div>
+					))}
+					<Button
+						onClick={() => {
+							updateSetting('Blocked', this.handleBlockChannel());
+						}}
+					>
+						Add channel
+					</Button>
+				</Category>
+				<Category
+					name="Blur Channels"
+					description="Blur channels that aren`t affected."
+					opened={this.state.category1Opened}
+					onChange={() => this.setState({ category1Opened: !this.state.category1Opened })}
+				>
+					{blurChannels.map((channel, idx) => (
+						<div>
+							<TextInputWithButton
+								type="text"
+								placeholder={'Enter a channel/user id'}
+								defaultValue={channel.id}
+								onChange={val => {
+									updateSetting('Blur', this.handleBlurChannelChange(idx, val));
+								}}
+								buttonOnClick={() => {
+									updateSetting('Blur', this.handleRemoveBlurChannel(idx));
+								}}
+								buttonText="Remove"
+								buttonIcon="fal fa-minus"
+							/>
+						</div>
+					))}
+					<Button
+						onClick={() => {
+							updateSetting('Blur', this.handleBlurAddChannel());
+						}}
+					>
+						Add channel
+					</Button>
+				</Category>
+				<p style={{ color: '#b9bbbe' }}>(you will need to switch channel/dm for any changes to take effect)</p>
+			</>
+		);
+	}
+};
--- index.js
+++ index.js
@@ -4,45 +4,45 @@ const { getModule, React } = require('powercord/webpack');
 const settings = require('./Components/Settings');
 
 module.exports = class BlurNSFW extends Plugin {
-  async startPlugin() {
-    this.loadStylesheet('style.scss');
-    powercord.api.settings.registerSettings(this.entityID, {
-      category: this.entityID,
-      label: 'Blur NSFW',
-      render: (props) => React.createElement(settings, {
-        main: this,
-        ...props
-      })
-    });
-    this.injectBlur();
-  }
+	startPlugin() {
+		this.loadStylesheet('style.scss');
+		powercord.api.settings.registerSettings(this.entityID, {
+			category: this.entityID,
+			label: 'Blur NSFW',
+			render: settings,
+		});
+		this.safeZones = ['DM Channel', 'Guild Channel'];
+		this.injectBlur();
+	}
 
-  pluginWillUnload() {
-    powercord.api.settings.unregisterSettings(this.entityID);
-    uninject('pog-blurnsfw');
-  }
+	pluginWillUnload() {
+		powercord.api.settings.unregisterSettings(this.entityID);
+		uninject('pog-blurnsfw');
+	}
 
-  blurChannel() {
-    const blur = this.settings.get('blurEffect', 10);
-    const timing = this.settings.get('blurTiming', 1);
-    var element = document.querySelector('.scrollerInner-2YIMLh');
-    if (element === null) return;
-    element.style.setProperty('--blur-effect', `blur(${blur}px)`);
-    element.style.setProperty('--blur-timing', `${timing}s`);
-    element.classList.toggle('blur', true);
-  }
+	blurChannel() {
+		const blur = this.settings.get('blurEffect', 10);
+		const timing = this.settings.get('blurTiming', 1);
+		var element = document.querySelector('.scrollerInner-2YIMLh');
+		if (element === null) return;
+		element.style.setProperty('--blur-effect', `blur(${blur}px)`);
+		element.style.setProperty('--blur-timing', `${timing}s`);
+		element.classList.add('blur');
+	}
 
-  async injectBlur() {
-    const channelTextArea = await getModule(m => m.type && m.type.render && m.type.render.displayName === "ChannelTextAreaContainer", false);
-    inject('pog-blurnsfw', channelTextArea.type, 'render', (_, res) => {
-      const channel = res.props.children[0].props.children[4].props.channel;
-      const blockedChannels = this.settings.get('Blocked') !== undefined ? this.settings.get('Blocked').filter((obj, idx) => obj.id === channel.id || obj.id === channel.recipients[0]) : '';
-      const blurChannels = this.settings.get('Blur') !== undefined ? this.settings.get('Blur').filter((obj, idx) => obj.id === channel.id || obj.id === channel.recipients[0]) : '';
-      if (blockedChannels.length !== 0) return res;
-      else if (blurChannels.length !== 0) this.blurChannel();
-      else if (this.settings.get('blurInDm') && channel.type === 1 || this.settings.get('blurInGroup') && channel.type === 3) this.blurChannel();
-      else if (channel.nsfw && channel.type === 0) this.blurChannel();
-      return res;
-    })
-  }
-}
+	async injectBlur() {
+		const channelTextArea = await getModule(m => m.type && m.type.render && m.type.render.displayName === 'ChannelTextAreaContainer', false);
+		inject('pog-blurnsfw', channelTextArea.type, 'render', (args, res) => {
+			const channel = args[0].channel;
+			const blockedChannels =
+				this.settings.get('Blocked') !== undefined ? this.settings.get('Blocked').filter((obj, idx) => obj.id === channel.id || obj.id === channel.recipients[0]) : '';
+			const blurChannels =
+				this.settings.get('Blur') !== undefined ? this.settings.get('Blur').filter((obj, idx) => obj.id === channel.id || obj.id === channel.recipients[0]) : '';
+			if (blockedChannels.length !== 0) return res;
+			else if (blurChannels.length !== 0) this.blurChannel();
+			else if ((this.settings.get('blurInDm') && channel.type === 1) || (this.settings.get('blurInGroup') && channel.type === 3)) this.blurChannel();
+			else if (channel.nsfw && channel.type === 0) this.blurChannel();
+			return res;
+		});
+	}
+};
